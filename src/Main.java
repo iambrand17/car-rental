4c7b31c47727345d955784d1729f3edef154ca5c
umar mujawar <mujawarumar111@gmail.com>
2024-02-02 19:32:47 +0530

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,59 @@
+class car{
+    private String carId;
+    private String brand;
+    private String model;
+    private  double basicPrisePerDay;
+    private boolean isAvailabal;
+
+
+
+    public car(String carId,String brand,String model,double basicPrisePerDay,boolean isAvailabal){
+        this.carId=carId;
+        this.brand=brand;
+        this.model=model;
+        this.basicPrisePerDay=basicPrisePerDay;
+        this.isAvailabal=true;
+    }
+    public String getCarId(){
+        return carId;
+    }
+    public String getBrand(){
+        return brand;
+    }
+    public String getModel(){
+        return model;
+    }
+    public  double calculatePrice(int rentalDay){
+        return basicPrisePerDay* rentalDay;
+
+    }
+    public boolean isAvailabal(){
+        return isAvailabal;
+    }
+    public void rent(){
+        isAvailabal=false;
+    }
+    public void returnCar(){
+        isAvailabal=true;
+    }
+}
+class customer{
+    private String coustomerID;
+    private  String name;
+
+    public customer (String coustomerID,String name){
+        this.coustomerID=coustomerID;
+        this.name=name;
+
+    }
+    public String getCoustomerID(){
+        return  coustomerID;
+    }
+    public String getName(){
+        return name;
+    }
+}
+
+class Retal{
+
+}
